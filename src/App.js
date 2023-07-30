0978d63e5aa203d6c411b6bf5296df08bc1c56bc
AnastasiiaKorch <anastasia_92_92@list.ru>
2023-07-30 21:33:49 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,71 @@
+import React, { useState } from 'react';
 import moment from 'moment';
-import React, {useState} from 'react';
-import Moment from 'react-moment';
-
-function DateTimePretty(props) {
-  const newDate = moment(props.date).startOf('day').fromNow();
-
-  return(
-      <DateTime date = {newDate}/>
-  )
-
-}
+import 'moment/locale/ru';
 
 function DateTime(props) {
-  return (
-      <p className="date">{props.date}</p>
-  )
+    return (
+        <p className="date">{props.date}</p>
+    )
 }
 
+function withDateTimePretty(Component) {
+
+    return class extends React.Component {
+        state = {}
+        componentDidMount() {
+            const format = "YYYY-MM-DD HH:mm:ss LT";
+            this.setState({date: moment(this.props.date, format).locale('ru').fromNow()});
+        }
+        render() {
+            return <Component {...this.props} date={this.state.date} />
+        }
+    }
+}
+
+const DateTimePretty = withDateTimePretty(DateTime)
+
 function Video(props) {
-  return (
-      <div className="video">
-        <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        <DateTimePretty date={props.date} />
-      </div>
-  )
+    return (
+        <div className="video">
+            <iframe title={props.url} src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <DateTimePretty date={props.date} />
+        </div>
+    )
 }
 
 function VideoList(props) {
-  return props.list.map(item => <Video url={item.url} date={item.date} />);
+    return props.list.map(item => <Video url={item.url} date={item.date} />);
 }
 
 export default function App() {
-  const [list, setList] = useState([
-    {
-      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2023-04-20 13:24:00'
-    },
-    {
-      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2023-03-03 12:10:00'
-    },
-    {
-      url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2023-07-28 11:16:00'
-    },
-    {
-      url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2018-01-03 12:10:00'
-    },
-    {
-      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2018-01-01 16:17:00'
-    },
-    {
-      url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2017-12-02 05:24:00'
-    },
-  ]);
+    const [list] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2023-07-29 23:04:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2012-12-19 05:45:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2006-12-19 11:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2020-12-19 05:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-12-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2021-11-19 11:10:00'
+        },
+    ]);
 
-  return (
-      <VideoList list={list} />
-  );
+    return (
+        <VideoList list={list} />
+    );
 }
